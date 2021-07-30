843f374b7492056d9cbb94a805713bc0ef07b375
Amin <asabe041@uottawa.ca>
2021-07-30 10:47:09 -0400

    Updates

---
+++ precache-manifest.a23b15f6bdb4b22acdcb770c9a03eb99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f08b132bbca721a119fab52b09581d",
+    "url": "/PersonalWebsite/index.html"
+  },
+  {
+    "revision": "ee83314abefa31d4e65e",
+    "url": "/PersonalWebsite/static/css/main.2228fa9b.chunk.css"
+  },
+  {
+    "revision": "0e998a3b56bdd5c3579e",
+    "url": "/PersonalWebsite/static/js/2.43fac461.chunk.js"
+  },
+  {
+    "revision": "1a99f31cce55521ed49abfdcd2fc6d7c",
+    "url": "/PersonalWebsite/static/js/2.43fac461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee83314abefa31d4e65e",
+    "url": "/PersonalWebsite/static/js/main.d6d61972.chunk.js"
+  },
+  {
+    "revision": "fce5c8cf00063aa895cd",
+    "url": "/PersonalWebsite/static/js/runtime-main.cbb3f65e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/asabe041.github.io/precache-manifest.6519da714ce2705c34cfe25ecb0920d5.js"
+  "/PersonalWebsite/precache-manifest.a23b15f6bdb4b22acdcb770c9a03eb99.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/asabe041.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PersonalWebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
