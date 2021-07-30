536c1ddbb9bd821bbdacc148d52533a21a5c6b45
Amin <asabe041@uottawa.ca>
2021-07-30 10:45:41 -0400

    Updates

---
+++ precache-manifest.6519da714ce2705c34cfe25ecb0920d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b913c85929c227d69c800a7668b73904",
+    "url": "/asabe041.github.io/index.html"
+  },
+  {
+    "revision": "50fd3b2a4982e858cd3a",
+    "url": "/asabe041.github.io/static/css/main.2228fa9b.chunk.css"
+  },
+  {
+    "revision": "aa017bcafb072e0dee9d",
+    "url": "/asabe041.github.io/static/js/2.aee4e8e2.chunk.js"
+  },
+  {
+    "revision": "1a99f31cce55521ed49abfdcd2fc6d7c",
+    "url": "/asabe041.github.io/static/js/2.aee4e8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50fd3b2a4982e858cd3a",
+    "url": "/asabe041.github.io/static/js/main.26037305.chunk.js"
+  },
+  {
+    "revision": "dc9f9c2588e1c8737bc5",
+    "url": "/asabe041.github.io/static/js/runtime-main.e81dfc17.js"
+  }
+]);
