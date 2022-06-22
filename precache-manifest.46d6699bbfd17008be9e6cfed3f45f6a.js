3f4c1681df066e609f4d0cd99746bb173410ce2c
Amin <asabe041@uottawa.ca>
2022-06-22 00:05:42 -0400

    Updates

---
+++ precache-manifest.46d6699bbfd17008be9e6cfed3f45f6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2206cad5bf9ecbadde36",
+    "url": "/static/css/main.c4ca9c5d.chunk.css"
+  },
+  {
+    "revision": "2206cad5bf9ecbadde36",
+    "url": "/static/js/main.2206cad5.chunk.js"
+  },
+  {
+    "revision": "1ffc47a675d0ede9c6f0",
+    "url": "/static/js/1.1ffc47a6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9eb84a2402ba5831efb0abd2006dd4ba",
+    "url": "/index.html"
+  }
+];
